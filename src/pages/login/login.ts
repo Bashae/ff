05a7000375a653c257de52fbb4e40a917edd8db8
bashaen <ice.andrew.media@gmail.com>
2018-09-09 19:01:43 -0400

    add images

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
+import { HomePage } from '../home/home';
+import { PostsPage } from '../posts/posts';
 
 @IonicPage()
 @Component({
@@ -25,7 +27,10 @@ export class LoginPage {
 
     this.auth.signInWithEmail(credentials)
       .then(
-        (res) => { console.log(res) },
+        (res) => { 
+          console.log(res);
+          this.navCtrl.push(PostsPage);
+        },
         (err) => { console.log(err) }
       )
   }
--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
-import AP = firebase.auth.AuthProvider;
 
 @Injectable()
 export class AuthProvider {
   public user: firebase.User;
   public isLoggedIn: boolean = false;
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(
+    public afAuth: AngularFireAuth
+    ) {
     afAuth.authState.subscribe(user => {
       this.user = user;
       if(user) {
@@ -18,12 +19,10 @@ export class AuthProvider {
   }
 
   signInWithEmail(credentials) {
-    console.log('sign in with email');
     return this.afAuth.auth.signInWithEmailAndPassword(credentials.email, credentials.password);
   }
 
   signUpWithEmail(credentials) {
-    console.log('sign up with email');
     return this.afAuth.auth.createUserWithEmailAndPassword(credentials.email, credentials.password);
   }
 
